79af112b0d59c4fc008f8c819a186a77d97dcaa9
SarahSchmaier <sarahschmaier@sarahs-mbp-2.andover.edu>
2016-02-25 17:11:18 -0500

    fix file location

--- static/scripts.js
+++ static/scripts.js
@@ -1,7 +1,6 @@
 
 
-var names = [["AlexaPagano", "Alexa Pagano '16"], ["CharlotteStandefer", "Charlotte Standefer '16"],
-["AlexBidwell", "Alex Bidwell '17"], ["SarahAlice", "Alice Ahn '16 and Sarah Schmaier '16"]];
+var names = [["ChloeSarah", "Me and Chloe Lee '17 in front of my dorm: Stevens"], ["AddisonEvent", "Addison Ambassadors Event '15: Black and White Night"], ["CommonsSunset", "Commons at Sunset"]];
 
 
 
@@ -16,6 +15,10 @@ $(document).ready(function() {
 		var picsrc = "static/images/" + name[0] + ".jpg";
 		var pic = $("<img/>", {
 			src: picsrc
+		}).appendTo(sectiondiv);
+		var caption = $("<p/>", {
+			innerHTML: names[1],
+			id: name[0] + "caption",
 		}).appendTo(sectiondiv);
 		sectiondiv.appendTo("#pagepiling");
 	}
