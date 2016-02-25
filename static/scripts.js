b9d52b8a7b60298026eca0ffddda019be78e19a1
SarahSchmaier <sarahschmaier@sarahs-mbp-2.andover.edu>
2016-02-25 18:51:40 -0500

    Thursday changes - Nic session

--- static/scripts.js
+++ static/scripts.js
@@ -2,8 +2,6 @@
 
 var names = [["ChloeSarah", "Me and Chloe Lee '17 in front of my dorm: Stevens"], ["AddisonEvent", "Addison Ambassadors Event '15: Black and White Night"], ["CommonsSunset", "Commons at Sunset"]];
 
-
-
 $(document).ready(function() {
 	for (var i = 0; i<names.length; i++) {
 		var name = names[i];
@@ -11,18 +9,36 @@ $(document).ready(function() {
 		var sectiondiv = $("<div/>", {
 			class: "section",
 			id: name[0],
-		});
+		}).appendTo("#pagepiling");
+		var coldiv1 = $("<div/>", {
+			class: "col-xs-6",
+		}).appendTo(sectiondiv);
+		var coldiv2 = $("<div/>", {
+			class: "col-xs-6",
+		}).appendTo(sectiondiv);
+		var button = $("<button/>", {
+			class: "button",
+			text: "Blah",
+		}).appendTo(coldiv2);
+		var textdiv = $("<div/>", {
+			class: "textdiv"
+		}).html("Bloo blah Bloo").hide().appendTo(button);
 		var picsrc = "static/images/" + name[0] + ".jpg";
 		var pic = $("<img/>", {
 			src: picsrc
-		}).appendTo(sectiondiv);
+		}).appendTo(coldiv1);
 		var caption = $("<p/>", {
-			innerHTML: names[1],
 			id: name[0] + "caption",
-		}).appendTo(sectiondiv);
-		sectiondiv.appendTo("#pagepiling");
+		}).html(name[1]).appendTo(coldiv1);
 	}
+	$(".button").click(function(){
+		$(this).children(".textdiv").toggle();
+	});
     $('#pagepiling').pagepiling({
     	 verticalCentered: true,
     });
+    $(window).scroll(function(){
+		$(".container").hide();
+	});
 });
+
